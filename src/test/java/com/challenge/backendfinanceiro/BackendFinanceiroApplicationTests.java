1d9fac9a822624c37979da74fc666684e056b1ac
GabrielTavares96 <gabriel_sbo1996@hotmail.com>
2022-01-20 11:19:22 -0300

    first commit

---
+++ src/test/java/com/challenge/backendfinanceiro/BackendFinanceiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.backendfinanceiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendFinanceiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
